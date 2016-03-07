79c93789226d7bb61ac441ea0a906d7688fe56ba
Kaeny Ito-Cole <kaenyitocole@gmail.com>
2016-03-07 12:27:46 -0800

    Renamed Files

---
+++ MapTempConverter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  MapTempConverter
+//
+//  Created by Kaeny Ito-Cole on 2/28/16.
+//  Copyright © 2016 Kaeny Ito-Cole. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        //Always call the super implementation of viewDidLoad
+        super.viewDidLoad()
+        
+    }
+}
---
+++ MapTempConverter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MapTempConverter
+//
+//  Created by Kaeny Ito-Cole on 1/27/16.
+//  Copyright © 2016 Kaeny Ito-Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
